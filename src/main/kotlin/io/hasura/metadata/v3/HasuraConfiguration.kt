884e172170ae6de5831c4d77583d07a494429847
Balazs E. Pataki <pataki@gmail.com>
2022-11-04 09:43:14 +0100

    Fix tests
    Add mandatory source field to action object type defs.

--- src/main/kotlin/io/hasura/metadata/v3/HasuraConfiguration.kt
+++ src/main/kotlin/io/hasura/metadata/v3/HasuraConfiguration.kt
@@ -1,5 +1,6 @@
 package io.hasura.metadata.v3
 
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.*
 
@@ -115,3 +116,286 @@ val HasuraMetadataV3.replaceMetadataJson: String
     }.toString()
 
 fun List<JsonObject>.toJsonArray() = buildJsonArray { forEach { add(it) } }
+
+/**
+ * Merge `other` metadata into
+ */
+fun HasuraMetadataV3.merge(other: HasuraMetadataV3) : HasuraMetadataV3 {
+    TODO("Not tested")
+
+    // Work on a clone of the receiver
+    val orig = Json.decodeFromString<HasuraMetadataV3>(Json.encodeToString(this))
+
+    other.actions?.let {
+        if (orig.actions == null) {
+            orig.actions = other.actions
+        }
+        else {
+            orig.actions = buildList {
+                addAll(orig.actions!!)
+                addAll(other.actions!!)
+            }
+        }
+    }
+
+    other.allowlist?.let {
+        if (orig.allowlist == null) {
+            orig.allowlist = other.allowlist
+        }
+        else {
+            orig.allowlist = buildList {
+                addAll(orig.allowlist!!)
+                addAll(other.allowlist!!)
+            }
+        }
+    }
+
+    other.cronTriggers?.let {
+        if (orig.cronTriggers == null) {
+            orig.cronTriggers = other.cronTriggers
+        }
+        else {
+            orig.cronTriggers = buildList {
+                addAll(orig.cronTriggers!!)
+                addAll(other.cronTriggers!!)
+            }
+        }
+    }
+
+
+    other.customTypes?.let {
+        if (orig.customTypes == null) {
+            other.customTypes = orig.customTypes
+        }
+        else {
+            other.customTypes!!.scalars?.let {
+                if (orig.customTypes!!.scalars == null) {
+                    orig.customTypes!!.scalars = other.customTypes!!.scalars
+                }
+                else {
+                    orig.customTypes!!.scalars = buildList {
+                        addAll(orig.customTypes!!.scalars!!)
+                        addAll(other.customTypes!!.scalars!!)
+                    }
+                }
+            }
+            other.customTypes!!.enums?.let {
+                if (orig.customTypes!!.enums == null) {
+                    orig.customTypes!!.enums = other.customTypes!!.enums
+                }
+                else {
+                    orig.customTypes!!.enums = buildList {
+                        addAll(orig.customTypes!!.enums!!)
+                        addAll(other.customTypes!!.enums!!)
+                    }
+                }
+            }
+            other.customTypes!!.objects?.let {
+                if (orig.customTypes!!.objects == null) {
+                    orig.customTypes!!.objects = other.customTypes!!.objects
+                }
+                else {
+                    orig.customTypes!!.objects = buildList {
+                        addAll(orig.customTypes!!.objects!!)
+                        addAll(other.customTypes!!.objects!!)
+                    }
+                }
+            }
+            other.customTypes!!.inputObjects?.let {
+                if (orig.customTypes!!.inputObjects == null) {
+                    orig.customTypes!!.inputObjects = other.customTypes!!.inputObjects
+                }
+                else {
+                    orig.customTypes!!.inputObjects = buildList {
+                        addAll(orig.customTypes!!.inputObjects!!)
+                        addAll(other.customTypes!!.inputObjects!!)
+                    }
+                }
+            }
+        }
+    }
+
+    other.inheritedRoles?.let {
+        if (orig.inheritedRoles == null) {
+            orig.inheritedRoles = other.inheritedRoles
+        }
+        else {
+            orig.inheritedRoles = buildList {
+                addAll(orig.inheritedRoles!!)
+                addAll(other.inheritedRoles!!)
+            }
+        }
+    }
+
+    other.queryCollections?.let {
+        if (orig.queryCollections == null) {
+            orig.queryCollections = other.queryCollections
+        }
+        else {
+            orig.queryCollections = buildList {
+                addAll(orig.queryCollections!!)
+                addAll(other.queryCollections!!)
+            }
+        }
+    }
+
+    other.remoteSchemas?.let {
+        if (orig.remoteSchemas == null) {
+            orig.remoteSchemas = other.remoteSchemas
+        }
+        else {
+            orig.remoteSchemas = buildList {
+                addAll(orig.remoteSchemas!!)
+                addAll(other.remoteSchemas!!)
+            }
+        }
+    }
+
+    other.restEndpoints?.let {
+        if (orig.restEndpoints == null) {
+            orig.restEndpoints = other.restEndpoints
+        }
+        else {
+            orig.restEndpoints = buildList {
+                addAll(orig.restEndpoints!!)
+                addAll(other.restEndpoints!!)
+            }
+        }
+    }
+
+    val origSourceNames = orig.sources.map { it.name }.toSet()
+    val otherSourceNames = other.sources.map { it.name }.toSet()
+
+    val sourceList = mutableListOf<Source>()
+
+    // 1. add all sources that are in orig but not in other
+    val origOnlyNames = origSourceNames.minus(otherSourceNames)
+    orig.sources
+        .filter { origOnlyNames.contains(it.name) }
+        .forEach { sourceList.add(it) }
+
+    // 2. add all sources that are in other but not in orig
+    val otherOnlyNames = otherSourceNames.minus(origSourceNames)
+    other.sources
+        .filter { otherOnlyNames.contains(it.name) }
+        .forEach { sourceList.add(it) }
+
+    // Merge source, which appear in both
+    val commonNames = origSourceNames.intersect(otherOnlyNames)
+    val commonSources = orig.sources.filter { commonNames.contains(it.name) }
+    commonSources.forEach {origSource ->
+        val otherSource = other.sources.filter { it.name == origSource.name }.first()
+
+        otherSource.tables?.let {
+            if (origSource.tables == null) {
+                origSource.tables = otherSource.tables
+            }
+            else {
+                origSource.tables = buildList {
+                    addAll(origSource.tables!!)
+                    addAll(otherSource.tables!!)
+                }
+            }
+        }
+
+        otherSource.functions?.let {
+            if (origSource.functions == null) {
+                origSource.functions = otherSource.functions
+            }
+            else {
+                origSource.functions = buildList {
+                    addAll(origSource.functions!!)
+                    addAll(otherSource.functions!!)
+                }
+            }
+        }
+
+        // Now everything is merged in origSource
+        sourceList.add(origSource)
+    }
+
+    // Update sources with new sources from other and the merged sources
+    orig.sources = sourceList
+
+    return this
+}
+
+fun HasuraConfiguration.merge(other: HasuraConfiguration): HasuraConfiguration {
+    TODO("Not tested")
+
+    // Work on a clone of the receiver
+    val orig = Json.decodeFromString<HasuraConfiguration>(Json.encodeToString(this))
+
+    orig.metadata.merge(other.metadata)
+
+    other.enumTableConfigs?.let {
+        if (orig.enumTableConfigs == null) {
+            orig.enumTableConfigs = other.enumTableConfigs
+        }
+        else {
+            orig.enumTableConfigs = buildList {
+                addAll(orig.enumTableConfigs!!)
+                addAll(other.enumTableConfigs!!)
+            }
+        }
+    }
+
+    other.computedFieldConfigs?.let {
+        if (orig.computedFieldConfigs == null) {
+            orig.computedFieldConfigs = other.computedFieldConfigs
+        }
+        else {
+            orig.computedFieldConfigs = buildList {
+                addAll(orig.computedFieldConfigs!!)
+                addAll(other.computedFieldConfigs!!)
+            }
+        }
+    }
+
+    other.cascadeDeleteFieldConfigs?.let {
+        if (orig.cascadeDeleteFieldConfigs == null) {
+            orig.cascadeDeleteFieldConfigs = other.cascadeDeleteFieldConfigs
+        }
+        else {
+            orig.cascadeDeleteFieldConfigs = buildList {
+                addAll(orig.cascadeDeleteFieldConfigs!!)
+                addAll(other.cascadeDeleteFieldConfigs!!)
+            }
+        }
+    }
+
+    // TODD: merge jsonSchema
+
+    return this
+}
+
+
+data class TablesAndFunctions(
+    var functions: List<CustomFunction> = listOf<CustomFunction>(),
+    var tables: List<TableEntry> = listOf<TableEntry>()
+)
+/**
+ * Returns from teh given source the tables and functions matching the given schemaName
+ */
+fun HasuraMetadataV3.tablesAndFunctionsWithSchema(sourceName: String, schemaName: String) : TablesAndFunctions {
+
+    val res = TablesAndFunctions()
+
+    sources?.find{ it.name == sourceName }?.let {source ->
+        val tablesInSchema = source.tables.filter {
+            it.table.schema == schemaName
+        }
+        val functionsInSchema = source.functions?.filter {
+            it.function is FunctionName.QualifiedFunctionValue &&
+                    (it.function as FunctionName.QualifiedFunctionValue).value.schema == schemaName
+        }
+        if (tablesInSchema.isNotEmpty()) {
+            res.tables = tablesInSchema
+        }
+        if (!functionsInSchema.isNullOrEmpty()) {
+            res.functions = functionsInSchema
+        }
+    }
+
+    return res
+}
