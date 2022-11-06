968c8ec11f4b4feaf0b8482b87b4231f0de7776e
Balazs E. Pataki <pataki@gmail.com>
2022-11-06 19:00:50 +0100

    Add scanBasePackageClasses to TestApp
    This is to show as an example how to use HasuraActionFilter in an app using
    hasuraconf.

--- src/test/kotlin/com/beepsoft/hasuraconf/TestApp.kt
+++ src/test/kotlin/com/beepsoft/hasuraconf/TestApp.kt
@@ -1,10 +1,11 @@
 package com.beepsoft.hasuraconf
 
+import com.beepsoft.hasura.actions.HasuraActionFilter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 // A dummy to bootstrap all the spring boot mechanisms for testing
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = [TestApp::class, HasuraActionFilter::class])
 class TestApp
 
 fun main(args: Array<String>) {
